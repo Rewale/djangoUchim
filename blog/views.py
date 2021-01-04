936dbd28cf22ffc792a2713906bb057dcaeba8c2
Rewale <komaroff.sania2015@gmail.com>
2021-01-04 23:43:16 +0500

    TEMPLETES

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def post_list(requset):
-    return render(requset, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(requset, 'blog/post_list.html', {'posts': posts})
